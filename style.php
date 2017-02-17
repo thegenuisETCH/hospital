3c1efe166bc187bf3ffa239aea315b3b83215d5e
Hesham Hanafy <hesham_hanafy44@yahoo.com>
2017-02-17 03:31:25 +0200

    setup

---
+++ style.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
